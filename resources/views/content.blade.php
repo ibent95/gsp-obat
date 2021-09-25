35e83a65c3378cdc78f518781a4b1ce3b2f5a0a9
Ibnu Tuharea <ibent95tuny@gmail.com>
2021-09-25 20:02:39 +0700

    Fix template

--- resources/views/content.blade.php
+++ resources/views/content.blade.php
@@ -1,6 +1,6 @@
 @php $content = (isset($_GET['content'])) ? $_GET['content'] : 'home' ; @endphp
 
-@if (file_exists('contents/' . $content . '.blade.php'))
+@if (file_exists(resource_path('/views/contents/' . $content . '.blade.php')))
 	@include('contents/' . $content )
 @else
 	@include('contents/404')
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -19,52 +19,63 @@
 	</div>
 	<!-- [ Pre-loader ] End -->
 
+	<!-- Header -->
 	@include('header')
 
-	<div class="container">
+	<!-- [ Main Content ] start -->
+	<div class="pc-container">
 
-		<!--@include('content')-->
+		@include('content')
 
-		<!-- Button trigger modal -->
-		<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#bootstrapModal" data-bs-whatever="@mdo" id="bootstrapModalButton">
-			Launch demo modal
-		</button>
+		<div class="pcoded-content">
+			<!-- Button trigger modal -->
+			<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#bootstrapModal" data-bs-whatever="@mdo" id="bootstrapModalButton">
+				Launch demo modal
+			</button>
 
-		<div class="modal fade" id="bootstrapModal" tabindex="-1" aria-labelledby="bootstrapModalLabel" aria-hidden="true" role="dialog">
-			<div class="modal-dialog">
-				<div class="modal-content">
-					<div class="modal-header">
-						<h5 class="modal-title" id="bootstrapModalLabel">New message</h5>
-						<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-					</div>
-					<div class="modal-body">
-						<form>
-							<div class="mb-3">
-								<label for="recipient-name" class="col-form-label">Recipient:</label>
-								<input type="text" class="form-control" id="recipient-name">
-							</div>
-							<div class="mb-3">
-								<label for="message-text" class="col-form-label">Message:</label>
-								<textarea class="form-control" id="message-text"></textarea>
-							</div>
-						</form>
-					</div>
-					<div class="modal-footer">
-						<button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-						<button type="button" class="btn btn-primary">Send message</button>
+			<div class="modal fade" id="bootstrapModal" tabindex="-1" aria-labelledby="bootstrapModalLabel" aria-hidden="true" role="dialog">
+				<div class="modal-dialog">
+					<div class="modal-content">
+						<div class="modal-header">
+							<h5 class="modal-title" id="bootstrapModalLabel">New message</h5>
+							<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+						</div>
+						<div class="modal-body">
+							<form>
+								<div class="mb-3">
+									<label for="recipient-name" class="col-form-label">Recipient:</label>
+									<input type="text" class="form-control" id="recipient-name">
+								</div>
+								<div class="mb-3">
+									<label for="message-text" class="col-form-label">Message:</label>
+									<textarea class="form-control" id="message-text"></textarea>
+								</div>
+							</form>
+						</div>
+						<div class="modal-footer">
+							<button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+							<button type="button" class="btn btn-primary">Send message</button>
+						</div>
 					</div>
 				</div>
 			</div>
+
+			<button type="button" class="btn btn-primary" id="sweetalertButton">
+				Launch demo SweetAlert2
+			</button>
 		</div>
 
-		<button type="button" class="btn btn-primary" id="sweetalertButton">
-			Launch demo SweetAlert2
-		</button>
-
 	</div>
+	<!-- [ Main Content ] end -->
 
-	@include('footer')
+	<!-- [ Footer ] start -->
+	<div class="pc-container">
+		<div class="pcoded-content">
+			@include('footer')
+		</div>
 	</div>
+	<!-- [ Footer ] end -->
+
 	<script src="assets/js/core.js"></script>
 	<script src="assets/js/vendor.js"></script>
 	<script src="assets/js/manifest.js"></script>
